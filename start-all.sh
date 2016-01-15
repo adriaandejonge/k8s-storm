29e652e01d8ab2e012413577409b601b33974dcf
Adriaan de Jonge <adriaandejonge@gmail.com>
2016-01-15 16:12:28 +0530

    Basic setup

---
+++ start-all.sh
@@ -0,0 +1,12 @@
+!#/bin/bash
+kubectl create -f svc-zookeeper.yaml
+kubectl create -f pod-zookeeper.yaml
+sleep 10
+kubectl create -f svc-storm-nimbus.yaml
+kubectl create -f pod-storm-nimbus.yaml
+sleep 10
+kubectl create -f svc-storm-supervisor.yaml
+kubectl create -f rc-storm-supervisor.yaml
+kubectl create -f svc-storm-ui.yaml
+kubectl create -f pod-storm-ui.yaml
+kubectl create -f pod-storm-examples.yaml
